d303404f37daf7b8228b4dbe8f286f4c902ae5da
鞠祥明 <nn880328@126.com>
2020-04-26 09:12:02 +0800

    增加401错误码处理

--- app.js
+++ app.js
@@ -46,6 +46,17 @@ app.use('/upload', uploadRouter);
 app.use('/admin', adminRouter);
 app.use('/role', roleRouter);
 app.use('/menu', menuRouter);
+
+// 处理401错误
+app.use(function (err, req, res, next) {
+	if (err.name === 'UnauthorizedError') {
+		res.status(401).json({
+			status: false,
+			...err,
+		});
+	}
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
 	next(createError(404));
