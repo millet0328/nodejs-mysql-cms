02e99edcc0865027f4c66cb06aa3c4fddd15a88a
鞠祥明 <nn880328@126.com>
2020-06-10 22:48:06 +0800

    关闭CORS,Token校验

--- app.js
+++ app.js
@@ -30,12 +30,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // 设置跨域资源分享CORS
-app.use(cors());
+// app.use(cors());
 
 //使用中间件验证token合法性
-app.use(expressJwt({ secret: 'secret' }).unless({
-	path: ['/', '/admin/register', '/admin/login'] //除了这些地址，其他的URL都需要验证
-}));
+//除了这些地址，其他的URL都需要验证
+// app.use(expressJwt({ secret: 'secret' }).unless({
+// 	path: ['/', '/admin/register', '/admin/login']
+// }));
 
 app.use('/', indexRouter);
 app.use('/user', userRouter);
@@ -58,12 +59,12 @@ app.use(function (err, req, res, next) {
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
 	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
