9b65d2e14047b14864f09b27e006b66b3bb98392
鞠祥明 <nn880328@126.com>
2022-05-01 23:30:16 +0800

    修改幻灯片字段usable

--- routes/blog/slide.js
+++ routes/blog/slide.js
@@ -11,21 +11,21 @@ let pool = require('../../config/mysql');
  *
  * @apiQuery { Number } [pagesize=10] 每一页幻灯片数量.
  * @apiQuery { Number } [pageindex=1] 第几页.
- * @apiQuery { Number=1,-1 } [available] 是否启用。1-启用，-1-禁用。
+ * @apiQuery { Number=1,-1 } [usable=1] 是否启用。1-启用，-1-禁用。
  *
  * @apiSuccess {Object[]} data 幻灯片数组.
  *
  * @apiSampleRequest /slide/list
  */
 router.get("/list", async (req, res) => {
-    let { pagesize = 10, pageindex = 1, available } = req.query;
+    let { pagesize = 10, pageindex = 1, usable = 1 } = req.query;
     // 计算偏移量
     pagesize = parseInt(pagesize);
     const offset = pagesize * (pageindex - 1);
     // 根据参数，拼接SQL
     let select_sql = 'SELECT *, DATE_FORMAT(create_date,"%Y-%m-%d %T") AS create_time FROM slide WHERE 1 = 1';
-    if (available) {
-        select_sql += ` AND available = ${available}`
+    if (usable) {
+        select_sql += ` AND usable = ${usable}`
     }
     select_sql += ' ORDER BY `slide_order` ASC LIMIT ? OFFSET ?;SELECT COUNT(*) as total FROM `slide`'
     // 判断启用/禁用状态
